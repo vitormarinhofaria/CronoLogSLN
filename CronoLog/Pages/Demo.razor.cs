6cb19e6ce9663ce5547b4cf3eb3a92a275c42527
Vitor Marinho Faria <vitormarinhofaria@gmail.com>
2021-08-30 18:32:02 -0300

    Pagina de demostração sem possibilidade de edição

---
+++ CronoLog/Pages/Demo.razor.cs
@@ -0,0 +1,123 @@
+﻿using BlazorDownloadFile;
+using CronoLog.Models;
+using CronoLog.Utils;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CronoLog.Pages
+{
+    public partial class Demo
+    {
+        private readonly string BoardId = "612d4666060ceb6976cb91a9";
+        [Inject]
+        public MongoClient DbClient { get; set; }
+        [Inject]
+        IBlazorDownloadFileService BlazorDownloadFileService { get; set; }
+        [Inject]
+        public IJSRuntime Js { get; set; }
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        public FullBoardData BoardData { get; set; }
+        protected string CurrentCardId { get; set; }
+        protected string CurrentMemberId { get; set; }
+
+        private Dictionary<string, Dictionary<string, List<CardTime>>> CardMemberTimers { get; set; }
+        public bool firstClick = true;
+        protected override async Task OnInitializedAsync()
+        {
+            CurrentCardId = "";
+            CurrentMemberId = "";
+            CardMemberTimers = new();
+            if (BoardId != string.Empty)
+            {
+                await GetFullBoardDataMulti();
+                foreach (var card in BoardData.Cards)
+                {
+                    if (!CardMemberTimers.ContainsKey(card.Id))
+                    {
+                        CardMemberTimers.Add(card.Id, new());
+                    }
+                    foreach (var timer in card.Timers)
+                    {
+                        if (!CardMemberTimers[card.Id].ContainsKey(timer.StartMember.Id))
+                        {
+                            CardMemberTimers[card.Id].Add(timer.StartMember.Id, new(card.Timers.Count + 1));
+                        }
+
+                        CardMemberTimers[card.Id][timer.StartMember.Id].Add(timer);
+                    }
+                }
+
+            }
+            await base.OnInitializedAsync();
+        }
+
+        private async Task GetFullBoardDataMulti()
+        {
+            var boardFilter = Builders<TrelloBoard>.Filter.Eq("Id", BoardId);
+            var board = await DatabaseUtils.BoardsCollection(DbClient).Find(boardFilter).FirstOrDefaultAsync();
+            BoardData = new FullBoardData
+            {
+                BoardId = board.Id,
+                BoardName = board.Name,
+                Members = board.Members
+            };
+            var cardsFilter = Builders<TrelloCard>.Filter.Eq("BoardId", BoardId);
+            var cards = await DatabaseUtils.CardsCollection(DbClient).Find(cardsFilter).ToListAsync();
+            var cardsList = new List<TrelloCard>();
+
+            foreach (var card in cards)
+            {
+                if (!card.CurrentList.Name.ToLower().Contains("geral") && !card.CurrentList.Name.ToLower().Contains("dúvidas") && !card.CurrentList.Name.ToLower().Contains("duvidas") && card.Active)
+                {
+                    foreach (var cTimer in card.Timers)
+                    {
+                        cTimer.Start = DateUtils.ToBrSpTimezone(cTimer.Start);
+                        cTimer.End = DateUtils.ToBrSpTimezone(cTimer.End);
+                    }
+                    cardsList.Add(card);
+                }
+            }
+            BoardData.Cards = cardsList;
+        }
+
+        public async void DownloadExcel()
+        {
+            var bytes = Controllers.WebAppController.GetExcelLocal(BoardId, DbClient, out string fileName);
+            var t = await BlazorDownloadFileService.DownloadFile(fileName, bytes, CancellationToken.None, "application/octet-stream");
+        }
+
+        private void RemoveChronoFromList(string cardId, string memberId, string chronoId)
+        {
+        }
+
+        private void SetCurrentSelected(TrelloCard card)
+        {
+            if (CurrentCardId != card.Id)
+            {
+                CurrentCardId = card.Id;
+                CurrentMemberId = string.Empty;
+            }
+            StateHasChanged();
+        }
+
+        private void SetCurrentMember(TrelloCard card, TrelloMember member)
+        {
+            firstClick = false;
+            CurrentMemberId = (CurrentMemberId == member.Id) ? string.Empty : member.Id;
+            CurrentCardId = card.Id;
+        }
+        private async void RefreshPage()
+        {
+            BoardData = null;
+            await OnInitializedAsync();
+            StateHasChanged();
+        }
+    }
+}
---
+++ CronoLog/Shared/ChronoItemDemo.razor.cs
@@ -0,0 +1,198 @@
+﻿using CronoLog.Models;
+using CronoLog.Utils;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CronoLog.Shared
+{
+    public partial class ChronoItemDemo
+    {
+        [Inject]
+        public MongoClient DbClient { get; set; }
+        [Parameter]
+        public bool FirstClick { get; set; }
+
+        [Parameter]
+        public CardTime Chrono { get; set; }
+
+        [Parameter]
+        public string CardId { get; set; }
+
+        [Parameter]
+        public List<TrelloMember> Members { get; set; }
+
+        [Parameter]
+        public string MemberId { get; set; }
+
+        [Parameter]
+        public Action<string, string, string> RemoveChrono { get; set; }
+
+        public TrelloMember SelectedMember { get; set; }
+
+        public UpdateChronoRequest updateRequest = new();
+
+        private bool isEditMode = false;
+        private bool requestHiddenConfirmation = false;
+        protected override async Task OnInitializedAsync()
+        {
+
+            updateRequest.CardId = CardId;
+            SelectedMember = Chrono.StartMember;
+            updateRequest.MemberId = Chrono.StartMember.Id;
+            updateRequest.RequestMemberId = MemberId;
+
+            Chrono.Start = Chrono.Start.AddSeconds(-Chrono.Start.Second);
+            Chrono.End = Chrono.End.AddSeconds(-Chrono.End.Second);
+            updateRequest.ChronoId = Chrono.Id;
+            updateRequest.Start = Chrono.Start;
+            updateRequest.End = Chrono.End;
+
+            await base.OnInitializedAsync();
+        }
+
+        public async void SaveBtn_()
+        {
+            var updateData = updateRequest;
+            var cardsCollection = DatabaseUtils.CardsCollection(DbClient);
+            var cardFilter = Builders<TrelloCard>.Filter.Eq("Id", updateData.CardId);
+            var card = await cardsCollection.Find(cardFilter).FirstOrDefaultAsync();
+
+            if (card != null)
+            {
+                var chrono = card.Timers.Find((t) => t.Id == updateData.ChronoId);
+                if (chrono != null)
+                {
+                    var boardsCollection = DatabaseUtils.BoardsCollection(DbClient);
+                    var boardFilter = Builders<TrelloBoard>.Filter.Eq("Id", card.BoardId);
+                    var board = await boardsCollection.Find(boardFilter).FirstOrDefaultAsync();
+                    var updateMember = board.Members.Find((m) => m.Id == updateData.MemberId);
+
+                    chrono.StartMember = updateMember;
+                    chrono.Start = DateUtils.ToDbSaveTime(updateData.Start);
+                    if (chrono.State != TimeState.RUNNING)
+                    {
+                        chrono.End = DateUtils.ToDbSaveTime(updateData.End);
+                    }
+                    await cardsCollection.FindOneAndReplaceAsync(cardFilter, card);
+
+                    //return new JsonResult("Alteraç\uc3a3o salva com sucesso!");
+                    await js.InvokeVoidAsync("alert", "Alteração salva com sucesso.");
+                }
+                else
+                {
+                    //return new JsonResult("Timer n\uc3a3o encontrado");
+                    await js.InvokeVoidAsync("alert", "Não foi possivel alterar esse Timer, erro na identificação do Timer");
+                }
+            }
+            else
+            {
+                //return new JsonResult("Cart\uc3a3o n\uc3a3o encontrado");
+                await js.InvokeVoidAsync("alert", "Não foi possivel alterar esse Timer, erro na identificação do cartão");
+            }
+        }
+
+        public async void SaveBtn()
+        {
+            updateRequest.Start = updateRequest.Start.ToUniversalTime();
+            updateRequest.End = updateRequest.End.ToUniversalTime();
+            var json = JsonSerializer.Serialize(updateRequest);
+            updateRequest.Start = Utils.DateUtils.ToBrSpTimezone(updateRequest.Start);
+            updateRequest.End = Utils.DateUtils.ToBrSpTimezone(updateRequest.End);
+
+            StringContent content = new(json);
+
+            Console.WriteLine(json);
+
+            var response = await httpC.PostAsJsonAsync($"{ApiUtils.API_URL}/webapp/chrono", updateRequest);
+            var responseString = await response.Content.ReadFromJsonAsync<string>();
+            var alertString = "";
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine(responseString);
+                if (responseString == "ascs")
+                {
+                    alertString = "Alteração salva com sucesso";
+                }
+                if (responseString == "nptvc")
+                {
+                    alertString = "Não é permitido alterar Timers que não pertencem a você";
+                }
+                if (responseString == "tne")
+                {
+                    alertString = "Timer não encontrado";
+                }
+                if (responseString == "cne")
+                {
+                    alertString = "Cartão não encontrado";
+                }
+                Console.WriteLine(alertString);
+            }
+            await js.InvokeVoidAsync("alert", alertString);
+
+        }
+        public async void DeleteChrono_()
+        {
+            var cardFilter = Builders<TrelloCard>.Filter.Eq("Id", CardId);
+            var card = await DatabaseUtils.CardsCollection(DbClient).Find(cardFilter).FirstOrDefaultAsync();
+            if (card != null)
+            {
+                var timer = card.Timers.Find(t => t.Id == Chrono.Id);
+                if (timer != null)
+                {
+                    card.Timers.Remove(timer);
+                    await DatabaseUtils.CardsCollection(DbClient).FindOneAndReplaceAsync(cardFilter, card);
+                    RemoveChrono(CardId, MemberId, Chrono.Id);
+                    await js.InvokeVoidAsync("alert", "Timer deletado com sucesso");
+                }
+                else
+                {
+                    await js.InvokeVoidAsync("alert", "Não foi possivel excluir esse Timer, ele não foi encontrado");
+                }
+            }
+            else
+            {
+                await js.InvokeVoidAsync("alert", "Não foi possivel excluir esse Timer, identificação do cartão invalida");
+            }
+        }
+        public async void DeleteChrono()
+        {
+            var response = await httpC.DeleteAsync($"{ApiUtils.API_URL}/webapp/chrono/{CardId}/{Chrono.Id}/{MemberId}");
+            var responseString = await response.Content.ReadFromJsonAsync<string>();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var alertString = "";
+                Console.WriteLine(responseString);
+                if (responseString == "ascs")
+                {
+                    alertString = "Alterado com sucesso";
+                    await js.InvokeVoidAsync("alert", "Alterado com sucesso");
+                    RemoveChrono(CardId, MemberId, Chrono.Id);
+                }
+                if (responseString == "nptvc")
+                {
+                    alertString = "Não é permitido alterar Timers que não pertencem a você";
+                    await js.InvokeVoidAsync("alert", "Não é permitido alterar Timers que não pertencem a você");
+                }
+                if (responseString == "tne")
+                {
+                    alertString = "Timer não encontrado";
+                    await js.InvokeVoidAsync("alert", alertString);
+                }
+                if (responseString == "cne")
+                {
+                    alertString = "Cartão não encontrado";
+                    await js.InvokeVoidAsync("alert", alertString);
+                }
+                Console.WriteLine(alertString);
+            }
+
+        }
+    }
+}
